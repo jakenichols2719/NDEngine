5aa66d609ea1c16c22a694f043863fa9b00f4b44
Jake <jnichols2719@protonmail.com>
2020-11-13 21:45:25 -0700

    Component functions set up, text improved

--- ndconst.cpp
+++ ndconst.cpp
@@ -23,17 +23,57 @@ void drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
   if(idx >= d.ct) return; //return if idx is too high
   std::string line = d.list[idx];
   for(int n=0; n<line.length(); n++) {
-    if(line[n] == ' ') {
-      px_x += font.h; //increment by font HEIGHT
+    //base calculation is from ' ' = 0 (ascii 32)
+    int letter_idx = std::toupper(line[n])-32; //calculate letter_idx from string
+    if(letter_idx == 0) {
+      px_x += font.h; //increment x by font HEIGHT
       continue;
     }
-    int letter_idx = line[n]-65;
     drawSprite(font, px_x, px_y, letter_idx);
-    px_x += font.h; //incrememnt by font HEIGHT
+    px_x += font.h; //incrememnt x by font HEIGHT/2 for decent-looking spaces.
   }
 }
 
-const int O_DISP_X=256, O_DISP_Y=256, D_DISP_W=1664, O_DISP_H=824;
+/*
+ * Individually defining origin, dimension of components,
+ * as well as functions to draw at their positions
+*/
+const int O_DISP_X=256, O_DISP_Y=256, D_DISP_W=1665, D_DISP_H=768;
 void Display_drawSprite(Sprite x, int px_x, int px_y, int idx) {
   drawSprite(x, px_x+O_DISP_X, px_y+O_DISP_Y, idx);
+}
+void Display_drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
+  drawText(d, font, px_x+O_DISP_X, px_y+O_DISP_Y, idx);
+}
+
+const int O_DIA_X=256, O_DIA_Y=0, D_DIA_W=1664, D_DIA_H=256;
+void Dialog_drawSprite(Sprite x, int px_x, int px_y, int idx) {
+  drawSprite(x, px_x+O_DIA_X, px_y+O_DIA_Y, idx);
+}
+void Dialog_drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
+  drawText(d, font, px_x+O_DIA_X, px_y+O_DIA_Y, idx);
+}
+
+const int O_MEN_X=0, O_MEN_Y=256, D_MEN_W=256, D_MEN_H=768;
+void Menu_drawSprite(Sprite x, int px_x, int px_y, int idx) {
+  drawSprite(x, px_x+O_MEN_X, px_y+O_MEN_Y, idx);
+}
+void Menu_drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
+  drawText(d, font, px_x+O_MEN_X, px_y+O_MEN_Y, idx);
+}
+
+const int O_IMG_X=0, O_IMG_Y=0, D_IMG_W=256, D_IMG_H=256;
+void Image_drawSprite(Sprite x, int px_x, int px_y, int idx) {
+  drawSprite(x, px_x+O_IMG_X, px_y+O_IMG_Y, idx);
+}
+void Image_drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
+  drawText(d, font, px_x+O_IMG_X, px_y+O_IMG_Y, idx);
+}
+
+const int O_BAR_X=0, O_BAR_Y=1024, D_BAR_W=1920, D_BAR_H=56;
+void Bar_drawSprite(Sprite x, int px_x, int px_y, int idx) {
+  drawSprite(x, px_x+O_BAR_X, px_y+O_BAR_Y, idx);
+}
+void Bar_drawText(Text d, Sprite font, int px_x, int px_y, int idx) {
+  drawText(d, font, px_x+O_BAR_X, px_y+O_BAR_Y, idx);
 }
--- run.cpp
+++ run.cpp
@@ -13,9 +13,10 @@ void display(void)
   glRasterPos2i (100, 100);
   glPixelZoom(WIN_S,WIN_S);
   drawLayout();
-  Display_drawSprite(img, 0, 0);
-  drawText(dia, font, 288, 192, 0);
-  drawText(dia, font, 288, 144, 1);
+  //Display_drawSprite(img, 0, 0);
+  Display_drawText(dia, font, 288, 192, 0);
+  Display_drawText(dia, font, 288, 144, 1);
+  Display_drawSprite(font, 0, 0, 40);
   glutSwapBuffers();
 }
 
@@ -24,10 +25,10 @@ int main(int argc, char** argv)
   img.w = DIM_IMG;
   img.h = DIM_IMG;
   img.ct = 1;
-  img.list = loadImage("portrait");
-  font.w = 32;
-  font.h = 32;
-  font.ct = 26;
+  //img.list = loadImage("portrait");
+  font.w = 12;
+  font.h = 16;
+  font.ct = 64;
   font.list = loadImage("font");
   dia.ct = 2;
   dia.list = loadDialog("test");
